e793390a2b653353af6582687c2337a4ea0eea63
Ekalak01 <s6301012620103@kmutnb.ac.th>
2023-04-20 20:25:04 +0700

    update

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from paho.mqtt import client as mqtt_client
 app = Flask(__name__)
 CORS(app)
 
-door_status = {'locked': True}
+door_status = {'locked': True, 'peopleCount': 0, 'doorOpen': False}
 
 broker = 'test.mosquitto.org'
 port = 1883
@@ -14,11 +14,26 @@ topic = '123'
 def on_connect(client, userdata, flags, rc):
     if rc == 0:
         print("Connected to MQTT Broker!")
+        client.subscribe(topic)
     else:
         print("Failed to connect, return code %d\n", rc)
 
+def on_message(client, userdata, msg):
+    global door_status
+    message = msg.payload.decode()
+    
+    if message == 'INC':
+        door_status['peopleCount'] += 1
+    elif message == 'DEC':
+        door_status['peopleCount'] -= 1
+    elif message == 'DOOR_OPEN':
+        door_status['doorOpen'] = True
+    elif message == 'DOOR_CLOSED':
+        door_status['doorOpen'] = False
+        
 client = mqtt_client.Client()
 client.on_connect = on_connect
+client.on_message = on_message
 client.connect(broker, port)
 client.loop_start()
 
@@ -33,7 +48,7 @@ def toggle_lock():
 
     lock_status = 'LOCK' if door_status['locked'] else 'UNLOCK'
     client.publish(topic, lock_status)
-
+    
     return jsonify(door_status)
 
 if __name__ == '__main__':
